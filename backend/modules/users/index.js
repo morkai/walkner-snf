d77b673501d37f7d8cc7732f421e2abaac5aceaf
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2014-01-03 00:05:44 +0100

    Removed the unused `user.sync` command.

--- backend/modules/users/index.js
+++ backend/modules/users/index.js
@@ -1,15 +1,11 @@
 'use strict';
 
 var setUpRoutes = require('./routes');
-var setUpCommands = require('./commands');
 
 exports.DEFAULT_CONFIG = {
   mongooseId: 'mongoose',
   expressId: 'express',
-  userId: 'user',
-  sioId: 'sio',
-  companiesId: 'companies',
-  sqlConnStr: null
+  userId: 'user'
 };
 
 exports.start = function startUsersModule(app, module)
@@ -21,15 +17,5 @@ exports.start = function startUsersModule(app, module)
       module.config.expressId
     ],
     setUpRoutes.bind(null, app, module)
-  );
-
-  app.onModuleReady(
-    [
-      module.config.mongooseId,
-      module.config.userId,
-      module.config.sioId,
-      module.config.companiesId
-    ],
-    setUpCommands.bind(null, app, module)
   );
 };
