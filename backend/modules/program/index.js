fb75ba1bbbdd66fb34f0b3da5b13bd9a4d2ce020
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2014-01-02 18:47:14 +0100

    Added initial implementation of the `program` module.

---
+++ backend/modules/program/index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var lodash = require('lodash');
+
+exports.DEFAULT_CONFIG = {
+  modbusId: 'modbus'
+};
+
+exports.start = function startProgramModule(app, module)
+{
+  var modbus = app[module.config.modbusId];
+
+  if (!modbus)
+  {
+    throw new Error("modbus module is required!");
+  }
+
+  var config = module.config;
+  var execQueue = null;
+
+  module.exec = function(func, name)
+  {
+    if (execQueue === null)
+    {
+      execQueue = {};
+
+      process.nextTick(function()
+      {
+        var queue = execQueue;
+
+        execQueue = null;
+
+        lodash.each(queue, function(func) { func(); });
+      });
+    }
+
+    execQueue[name || func.name] = func;
+  };
+
+  var masterStatusTag = 'masters.controlProcess';
+
+  if (config.simulate || modbus.values[masterStatusTag])
+  {
+    init();
+  }
+  else
+  {
+    app.broker
+      .subscribe('tagValueChanged.' + masterStatusTag, init)
+      .setLimit(1);
+  }
+
+  function init()
+  {
+    app.timeout(1337, function()
+    {
+      // TODO
+    });
+  }
+};
