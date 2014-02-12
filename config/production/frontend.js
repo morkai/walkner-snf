e3e9dfa2eb4308fcfa07f45f441b513218013ca6
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2014-02-12 18:05:20 +0100

    Kill all instances of google-chrome before running a new one.

--- config/production/frontend.js
+++ config/production/frontend.js
@@ -11,6 +11,8 @@ config.httpsServer.port = 443;
 config.user.localAddresses = ['127.0.0'];
 
 config.exec = {
-  cmd: 'export DISPLAY=:0 && /usr/bin/google-chrome --user-data-dir=/root/chrome-profile --kiosk',
+  cmd: 'export DISPLAY=:0'
+    + ' ; killall google-chrome'
+    + ' ; /usr/bin/google-chrome --user-data-dir=/root/chrome-profile --kiosk',
   afterAppStarted: true
 };
