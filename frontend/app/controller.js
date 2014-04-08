76411ac1ab2673fb0dd32a8864d263a73591b668
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2014-04-08 19:33:30 +0200

    Changed 'Part of walkner-snf' to 'Part of the walkner-snf project'.

--- frontend/app/controller.js
+++ frontend/app/controller.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2014, Łukasz Walukiewicz <lukasz@walukiewicz.eu>. Some Rights Reserved.
 // Licensed under CC BY-NC-SA 4.0 <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
-// Part of walkner-snf <http://lukasz.walukiewicz.eu/p/walkner-snf>
+// Part of the walkner-snf project <http://lukasz.walukiewicz.eu/p/walkner-snf>
 
 define([
   './broker',
